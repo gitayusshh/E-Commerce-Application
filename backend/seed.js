f76d43d3d5491afe8a6f8b4e0b244e4963b1ec16
Ayush Agnihotri <ayushagnihotri1234567@gmail.com>
2026-09-05 12:58:46 +0530

    Fix Vercel deployment and remove node_modules

--- backend/seed.js
+++ backend/seed.js
@@ -4,7 +4,7 @@ import { connectDB } from "./config/db.js";
 import Category from "./models/Category.js";
 import Product from "./models/Product.js";
 import User from "./models/User.js";
-dotenv.config();
+dotenv.config();+
 await connectDB();
 const categories = [
   [
