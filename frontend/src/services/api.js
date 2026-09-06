28f85c21f9faa86a53fcf2889235e0ec1e879f24
Ayush Agnihotri <ayushagnihotri1234567@gmail.com>
2026-09-06 18:00:13 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,3 @@
-// import axios from "axios";
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
-// });
-// api.interceptors.request.use((c) => {
-//   const t = localStorage.getItem("token");
-//   if (t) c.headers.Authorization = `Bearer ${t}`;
-//   return c;
-// });
-// export default api;
 
 
 
